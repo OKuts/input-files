b57bf3cd6be7a1036641a3f2264f1500c648fa45
Ol_Kuts <kuts.ol.vas@gmail.com>
2022-01-11 05:46:09 +0200

    input file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [drag, setDrag] = useState(false)
+    const [cn, setCn] = useState('drop-area')
+
+    const dragStartHandler = (e) => {
+        e.preventDefault()
+        setDrag(true)
+        setCn('drop-area drop')
+    }
+
+    const dragLeaveHandler = (e) => {
+        e.preventDefault()
+        setDrag(false)
+        setCn('drop-area')
+    }
+
+    const onDropHandler = (e) => {
+        e.preventDefault()
+        let files = [...e.dataTransfer.files]
+        files.forEach((file, i) => {
+            const formData = new FormData()
+            formData.append(`file${i}`, file)
+            console.log(formData)
+            // send formData
+        })
+
+        setDrag(false)
+        setCn('drop-area')
+    }
+
+    const inputHandler = (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.target)
+        console.log(formData.get('file'))
+        // send formData
+    }
+
+    return (
+        <div className="app">
+                {drag
+                    ? <div className={cn}
+                           onDragOver={dragStartHandler}
+                           onDragLeave={dragLeaveHandler}
+                           onDragStart={dragStartHandler}
+                           onDrop={onDropHandler}>
+                        Отпусти файл
+                </div>
+                    : <div className={cn}
+                        onDragOver={dragStartHandler}
+                        onDragLeave={dragLeaveHandler}
+                        onDragStart={dragStartHandler} >
+                        Перемести файл сюда<br/>
+                        <form action="post" onSubmit={inputHandler}>
+                            <label htmlFor="file">Choose images to upload (TXT, PDF)</label>
+                            <input type="file" accept=".txt" id="file" name="file"/>
+                            <button>Send</button>
+                        </form>
+                    </div>}
+
+        </div>
+    );
 }
 
 export default App;
